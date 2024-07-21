d8e1ef32f013ca0ad7a845456bbcdf3d192dab28
Alexandre Rodrigues Silva <rodrialexandre@gmail.com>
2024-07-21 17:29:33 -0300

    Primeiro commit
    Signed-off-by: Alexandre Rodrigues Silva <rodrialexandre@gmail.com>

---
+++ src/main/java/br/com/xandy/h2tst/H2tstApplication.java
@@ -0,0 +1,13 @@
+package br.com.xandy.h2tst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2tstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2tstApplication.class, args);
+	}
+
+}
